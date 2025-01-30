3b62aef0507eda49ddb49e65af01c0873ef7632b
JuanJavier <juanjcbreton@gmail.com>
2025-01-30 14:54:27 -0600

    30-1-25 v4.0

--- home/views.py
+++ home/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Project
+
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    name = 'Juan Javier'
+    career = 'Backend Developer'
+    context = {
+        "name": name,
+        "career": career
+    }
+    return render(request, 'home/index.html', context)
 
 def contact(request):
     return render(request, 'home/contact.html')
 
 def projects(request):
-    return render(request, 'home/projects.html')
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, 'home/projects.html', context)
 
 def about(request):
-    return render(request, 'home/about.html')
+    name = 'Juan Javier Castillo Bretón'
+    description = 'Programador de todo tipo XDDDDDD'
+    context = {
+        "name": name,
+        "description": description
+    }
+    return render(request, 'home/about.html', context)
